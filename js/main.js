27da9a85c3805a9033cdcd4a633d5901d0789eab
Ricardo Martínez Hernández <89108049+ricardomtnez@users.noreply.github.com>
2023-05-23 09:42:00 -0600

    Archivos locales
    Archivos locales trabajados

---
+++ js/main.js
@@ -0,0 +1,84 @@
+/*
+Template Name: ClassiGrids - Classified Ads and Listing Website Template.
+Author: GrayGrids
+*/
+
+(function () {
+
+	"use strict";
+
+	//===== Prealoder ========
+
+	window.onload = function () {
+		window.setTimeout(fadeout, 200);
+	}
+
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+        } else {
+            header_navbar.classList.remove("sticky");
+        }
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+     //===== mobile-menu-btn
+	let navbarToggler = document.querySelector(".mobile-menu-btn");
+	navbarToggler.addEventListener('click', function () {
+		navbarToggler.classList.toggle("active");
+	});
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    // WOW active
+    new WOW().init();
+    
+})();
+
